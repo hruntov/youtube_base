cb0baba9f1edd43819d123a73285308d1010a56c
Kostiantyn Hruntov <hruntov@outlook.com>
2023-12-12 14:36:39 +0200

    Add flake8 to the poetry root

---
+++ youtube_base/youtubers/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from . import models
+
+
+class TestTemplateView(TemplateView):
+    template_name = "youtubers/test.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_data'] = models.TestModel.objects.all()
+        return context
