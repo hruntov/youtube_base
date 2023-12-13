e4567c0f8f56b4f504e10113a4abad760678a23d
Kostiantyn Hruntov <hruntov@outlook.com>
2023-12-13 17:38:07 +0200

    Test fail gha

--- website/tests/test_website.py
+++ website/tests/test_website.py
@@ -3,5 +3,5 @@ from website import __version__
 
 class TestClass:
     def test_version(self):
-        assert __version__ == "0.1.0"
+        assert __version__ == "0.2.0"
         print(f"Version {__version__} is correct.")
