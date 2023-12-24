5cff1ff961b02f2c3ff60d58b043d462a9599c68
hruntov <hruntov@outlook.com>
2023-12-24 19:08:46 +0200

    Add functional test with closed browser

--- website/tests/func_test.py
+++ website/tests/func_test.py
@@ -1,5 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class BasicInstallTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_install(self):
+        self.browser.get("http://localhost:8000/test")
+        self.assertIn("Test Template", self.browser.title)
+
+
+if __name__ == "__main__":
+    unittest.main()
