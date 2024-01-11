7113edbca47d89debef8967fbe70136f882b4e65
hruntov <hruntov@outlook.com>
2024-01-11 18:50:06 +0200

    Add base.html - base template for all pages with using bootstrap.

--- youtube_base/youtubers/urls.py
+++ youtube_base/youtubers/urls.py
@@ -3,10 +3,10 @@ from django.urls import include, path
 from youtubers import views
 
 from .views import AddYoutuberView
-from .views import HomeView as home
+from .views import HomeView
 
 urlpatterns = [
-    path('', home.home),
+    path('', HomeView.as_view(), name='home'),
     path('add_youtuber/', AddYoutuberView.as_view(), name='add_youtuber'),
     path('category_list/', views.CategoryList.as_view(), name='category_list'),
 ]
