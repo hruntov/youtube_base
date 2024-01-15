9d043eead9ff24d3bcc7cffe98e343a828901215
hruntov <hruntov@outlook.com>
2024-01-15 18:48:26 +0200

    Add the superuser for django in the docker container.

--- start_web.sh
+++ start_web.sh
@@ -2,4 +2,5 @@
 
 cd youtube_base && \
 python manage.py migrate && \
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell && \
 python manage.py runserver 0.0.0.0:8000
