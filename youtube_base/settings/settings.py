73e864693408340775fe6095104d91e48cbdd3b5
Kostiantyn Hruntov <hruntov@outlook.com>
2024-01-25 02:10:50 +0200

    Implement the session context for the search youtubers result.

--- youtube_base/settings/settings.py
+++ youtube_base/settings/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'youtubers',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- youtube_base/youtubers/urls.py
+++ youtube_base/youtubers/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+
 from youtubers import views
 
 from .views import AddYoutuberView, HomeView, YoutuberDetailView
