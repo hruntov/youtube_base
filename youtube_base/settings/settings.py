838e8f1559f89013d38bd9b74e9145cd794e1a1c
Kostiantyn Hruntov <hruntov@outlook.com>
2024-01-26 19:54:01 +0200

    Add the crispy_forms to the project. Update libs.

--- youtube_base/settings/settings.py
+++ youtube_base/settings/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap4',
+    'crispy_forms',
     'youtubers',
 ]
 
@@ -127,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
