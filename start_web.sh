cc9e16a47b4f685b556827ee2f096149c2dbacb1
hruntov <hruntov@outlook.com>
2024-03-08 01:09:29 +0200

    Add the test for adding tags.

--- start_web.sh
+++ start_web.sh
@@ -2,7 +2,6 @@
 
 cd youtube_base && \
 python manage.py migrate && \
-python manage.py loaddata youtubers/fixtures/categories.json && \
-python manage.py loaddata youtubers/fixtures/youtubers.json && \
+python manage.py loaddata youtubers/fixtures/data.json && \
 python manage.py loaddata users/fixtures/users.json | python manage.py shell
 python manage.py runserver 0.0.0.0:8000
