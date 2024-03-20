153c8e7dc5c6d0f59bed3a1b06f0b80b477c5b78
hruntov <hruntov@outlook.com>
2024-03-20 19:41:30 +0200

    Add fixture with the site

--- start_web.sh
+++ start_web.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd youtube_base && \
+python manage.py makemigrations && \
 python manage.py migrate && \
 python manage.py loaddata youtubers/fixtures/data.json && \
+python manage.py loaddata youtubers/fixtures/site.json && \
 python manage.py loaddata users/fixtures/users.json | python manage.py shell
 python manage.py runserver 0.0.0.0:8000
