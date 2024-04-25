513d182f6c20205f17a3f347bdd2cbee5e21bd83
hruntov <hruntov@outlook.com>
2024-04-25 17:02:18 +0300

    Use runserver_plus for running Django with SSL/TLS cert

--- start_web.sh
+++ start_web.sh
@@ -7,4 +7,4 @@ python manage.py loaddata youtubers/fixtures/youtubers.json && \
 python manage.py loaddata youtubers/fixtures/tags.json && \
 python manage.py loaddata users/fixtures/users.json && \
 python manage.py clearcache | python manage.py shell
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.crt
